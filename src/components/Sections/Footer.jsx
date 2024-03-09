12645051e845156d3f5d788318ca012ef4d7633e
dspwebstudiomx <dspwebstudio@gmail.com>
2024-03-09 11:32:17 -0600

    footer copyright change

--- src/components/Sections/Footer.jsx
+++ src/components/Sections/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
       {/*  Columns Container */}
       <section
-        className="container flex flex-col flex-wrap p-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap bg-slate-600">
+        className="container flex flex-col flex-wrap pt-10 lg:p-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap bg-slate-800">
 
         {/* Logo & Slogan Section */}
         <div className="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left lg:w-1/3">
@@ -35,8 +35,8 @@ const Footer = () => {
         {/* Logo & Slogan Section */}
 
         {/* Columns Section */}
-        <div className="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left lg:w-1/3">
-          <div className="w-full px-4 md:w-1/2">
+        <div className="flex flex-wrap flex-grow mt-10 text-center md:pl-20 md:mt-0 md:text-left lg:w-1/3">
+          <div className="w-1/2">
             <h2 className="mb-3 text-lg font-medium tracking-normal text-blue-500 uppercase title-font">About</h2>
             <nav className="mb-10 list-none">
               <li className="mt-3">
@@ -50,7 +50,7 @@ const Footer = () => {
               </li>
             </nav>
           </div>
-          <div className="w-full px-4 md:w-1/2">
+          <div className="px-4 w-1/2">
             <h2 className="mb-3 text-lg font-medium tracking-widest text-blue-500 uppercase title-font">Support</h2>
             <nav className="mb-10 list-none">
               <li className="mt-3">
@@ -73,7 +73,15 @@ const Footer = () => {
       {/* Copyright Section */}
       <section className="bg-slate-950 text-white">
         <div className="container px-5 py-4 mx-auto">
-          <p className="text-sm capitalize xl:text-center">© 2024 Todos los derechos reservados </p>
+          <p className="text-sm text-center">
+            <span>
+              © 2024 Todos los derechos reservados
+            </span>
+            <br />
+            <span>
+              dspwebstudio.com
+            </span>
+          </p>
         </div>
       </section>
       {/* Copyright Section */}
